b35bfb2682b7d157ee7dd378f2b8bf505fdc707d
Ryan Nave <ryannave97@gmail.com>
2026-04-29 15:40:07 -0700

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "BudgetBoard",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <body>{children}</body>
+      <Analytics/>
     </html>
   );
 }
